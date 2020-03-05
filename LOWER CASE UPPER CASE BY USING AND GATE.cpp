66cb2f8111bd765c36ff3644f5a60242f9e67562
aroona89 <61843416+aroona89@users.noreply.github.com>
2020-03-05 22:21:44 +0500

    Add files via upload

---
+++ LOWER CASE UPPER CASE BY USING AND GATE.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+   char ch;
+   cin>>ch;
+   if(ch>='a'&&ch<='z')
+   cout<<"character is in lower case";
+   else
+   cout<<"character is in upper case";
+	return 0;
+}
